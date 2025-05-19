e52ff555413afa129ee51742378f1649be381093
aliceeriksen <alice99@live.dk>
2025-05-19 12:00:23 +0200

    alice js

---
+++ alice.js
@@ -0,0 +1,34 @@
+let hamburgermenuens = [document.getElementById("minHamburgermenu")];
+let hamburgers = [document.querySelector(".hamburger")];
+
+function openNav(index) {
+    hamburgermenuens[index].style.width = "200px";      
+    document.addEventListener("click", closeNavOutside); 
+}
+
+function closeNav(index) {
+    hamburgermenuens[index].style.width = "0";
+    document.removeEventListener("click", closeNavOutside);
+}
+
+function closeNavOutside(event) {
+    hamburgermenuens.forEach((hamburgermenuen, index) => {
+        if (!hamburgermenuen.contains(event.target) && !hamburgers[index].contains(event.target)) {
+            closeNav(index);
+        }
+    });
+}
+
+let dropdown = document.querySelector('.dropdown-knappen');
+let dropdownIndhold = document.querySelector('.dropdown-indhold');
+
+dropdown.addEventListener('click', function(event) {
+    event.stopPropagation();
+    dropdownIndhold.style.display = dropdownIndhold.style.display === 'block' ? 'none' : 'block';
+});
+
+document.addEventListener('click', function(event) {
+    if (!dropdown.contains(event.target) && !dropdownIndhold.contains(event.target)) {
+        dropdownIndhold.style.display = 'none';
+    }
+});
